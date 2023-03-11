de6131b46fadd5341a865cae83ef87be6ccb9c7d
ThatGAN <74864357+ThatGAN@users.noreply.github.com>
2023-03-11 10:53:57 +0100

    Rewrite and Error Handling The Arduino is now on 0.16 Version of the SARA Firmware

--- Arduino & RaspberryPI/Arduino/mainMKR.ino
+++ Arduino & RaspberryPI/Arduino/mainMKR.ino
@@ -1,6 +1,8 @@
-// MQTT Import
+// MKR NarrowBand Import
 #include <MKRNB.h>
+// MQTT Import
 #include <ArduinoMqttClient.h>
+// Low Power Import
 #include <ArduinoLowPower.h>
 //Sensor Import
 #include <Arduino_MKRENV.h>
@@ -9,7 +11,7 @@ int sensorPin = A0;
 int sensorValue = 0;
 
 //The MKRNB lib needs this even if its blank
-const char PINNUMBER[]     = "1503";
+const char PINNUMBER[] = "1503";
 
 //broker url in here, demo is using eclipse test server
 
@@ -17,10 +19,9 @@ const char PINNUMBER[]     = "1503";
 int mqttPort = 1883;
 
 //var things for storage
-const char broker[] = "weatherstation.dynv6.net";
-int        port     = 1883;
-const char topic[]  = "MKREnvData";
-boolean loopTrigger = false;
+const char broker[] = "93.209.78.87";
+int port = 1883;
+const char topic[] = "MKREnvData";
 
 // initialize the library instance
 NBClient client;
@@ -29,11 +30,7 @@ NB nbAccess;
 
 MqttClient mqttClient(client);
 
-bool restart = false;
-bool connected = false;
-
-
-void setup() {  
+void setup() {
   //Setting up the Board Sensors
   setupSensors();
 
@@ -43,6 +40,8 @@ void setup() {
   //Setting up MQTT Connection
   setupMQTT();
 
+  pinMode(SARA_PWR_ON, OUTPUT);
+  digitalWrite(SARA_PWR_ON, HIGH);
 }
 
 void setupSensors() {
@@ -51,155 +50,168 @@ void setupSensors() {
 
   if (!ENV.begin()) {
     Serial.println("Failed to initialize MKR ENV shield!");
-    while (1);
+    while (1)
+      ;
   }
 }
 
-void setupNB(){
-   Serial.println("Warming up....");
-  // connection state
-  boolean connected = false;
+void setupNB() {
+  Serial.println("Warming up....");
 
+  // connection state
+  bool connected = false;
   while (!connected) {
-    if ((nbAccess.begin(PINNUMBER, true) == NB_READY) &&
-        (gprs.attachGPRS() == GPRS_READY)) {
-          Serial.println("Successfully connected!");
+    Serial.println("Start of !connected");
+    if (nbAccess.begin(PINNUMBER, true) == NB_READY){
+          Serial.println("Connect successful");
+          Serial.println("NB Access:");
+          Serial.println(nbAccess.isAccessAlive());
       connected = true;
     } else {
-      Serial.println("Not connected");
-      restart = true;
-      delay(1000);
+      Serial.println("Failed");
+      Serial.println("NB Access:");
+      Serial.println(nbAccess.isAccessAlive());
+      Serial.println("MqttConnect:");
+      Serial.println(mqttClient.connected());
+      delay(2000);
+      myReset();
     }
   }
-  
 }
-
 void setupMQTT() {
-  // initialize serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial && (millis() < 10000)) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
+  bool mqttConnected = false;
   Serial.println("connecting to the mqtt broker...");
 
   Serial.println(broker);
 
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
+while(!mqttConnected){
+  Serial.println("Start of !mqttConnected");
+  Serial.println("NB Access:");
+  Serial.println(nbAccess.isAccessAlive());
+  Serial.println("MqttConnect:");
+  Serial.println(mqttClient.connected());
+    if (!mqttClient.connect(broker, port)) {
+      Serial.print("MQTT connection failed! Error code = ");
+      Serial.println(mqttClient.connectError());
+      Serial.println("NB Access:");
+      Serial.println(nbAccess.isAccessAlive());
+      if(!nbAccess.isAccessAlive()){
+        myReset();
+      }
+      delay(2000);
+    } else {
+      Serial.println("Mqtt Success!");
+      Serial.println(mqttClient.connected());
+      mqttConnected = true;
+    }
   }
 
   Serial.println("You're connected to the MQTT broker!");
   Serial.println();
 
-  Serial.print("Subscribing to topic: ");
+  Serial.print("Publishing to topic: ");
   Serial.println(topic);
   Serial.println();
-
-  // subscribe to a topic
-  mqttClient.subscribe(topic);
-
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-
-  Serial.print("Waiting for messages on topic: ");
-  Serial.println(topic);
-  Serial.println();
-
-}
-
-void verifyConnection() {
-  if (nbAccess.isAccessAlive()){
-    connected = true;
-    Serial.println("Connection verified");
-  }
-    else{
-      connected = false;
-      Serial.println(F("Restart"));
-
-      setupNB();
-    }
-}
-
-void verifyMQTT(){
-  if(!mqttClient.connected()){
-    setupMQTT
-    ();
-  }else{
-    Serial.println("MQTT verified");
-  }
-}
-
-void restartSerial(){
-  if (!Serial){
-    Serial.begin(9600);
-    while(!Serial && (millis() < 10000)){
-      }
-  }
-  
 }
 
 void loop() {
   Serial.println("Start of loop()");
+    Serial.println("NB Access:");
+    Serial.println(nbAccess.isAccessAlive());
+    Serial.println("MqttConnect:");
+    Serial.println(mqttClient.connected());
 
-  //restartSerial();
-
+  //Check to see if the Connections are alive
+  //This defaults to connect after wakeUp
   verifyConnection();
   verifyMQTT();
-  
-  // put your main code here, to run repeatedly:
+
   // read all the sensor values
   float temperature = ENV.readTemperature();
-  float humidity    = ENV.readHumidity();
-  float pressure    = ENV.readPressure();
+  float humidity = ENV.readHumidity();
+  float pressure = ENV.readPressure();
   float illuminance = ENV.readIlluminance();
-  sensorValue = analogRead(sensorPin);     
+  sensorValue = analogRead(sensorPin);
 
   //Establishing MQTT Conn to send msg
   mqttClient.beginMessage(topic);
-  // print each of the sensor values
+  //print each of the sensor values
   //mqttClient.print("Temperature = ");
   mqttClient.print(temperature);
   mqttClient.print(",");
- // mqttClient.print(" °C");
+  // mqttClient.print(" °C");
 
- // mqttClient.print("Humidity    = ");
+  // mqttClient.print("Humidity    = ");
   mqttClient.print(humidity);
   mqttClient.print(",");
- // mqttClient.print(" %");
+  // mqttClient.print(" %");
 
- // mqttClient.print("Pressure    = ");
+  // mqttClient.print("Pressure    = ");
   mqttClient.print(pressure);
   mqttClient.print(",");
- // mqttClient.print(" kPa");
+  // mqttClient.print(" kPa");
 
-//  mqttClient.print("Illuminance = ");
+  //  mqttClient.print("Illuminance = ");
   mqttClient.print(illuminance);
   mqttClient.print(",");
- // mqttClient.print(" lx");
+  // mqttClient.print(" lx");
 
   mqttClient.print(sensorValue);
-  delay(10000);
   mqttClient.print(",");
-  
+
   mqttClient.print("638b772697742e380fdf5302");
   //Ending Connection
   mqttClient.endMessage();
-  
 
   delay(1000);
 
-  
+  //Clean Exit for MQTT
   mqttClient.stop();
 
   Serial.println("Entering Sleep WakeUp Cicle");
   Serial.end();
   //delay(10000);
-  nbAccess.shutdown();
-  
-  LowPower.sleep(600000);
 
+  //Shutdown the SARA Modem to save battery life
+  nbAccess.shutdown();
+
+  LowPower.sleep(600000);
+}
+
+//soft reset for Arduino if the Modem defaults into Error Loop
+void myReset(){
+  Serial.println("Reseting");
+  //Sleep for 1min so the Modem has time to rest
+  LowPower.sleep(60000);
+  NVIC_SystemReset();
+}
+
+void verifyConnection() {
+  if(!nbAccess.isAccessAlive()){
+    Serial.println("Connection not verified");
+    setupNB();
+  }else{
+    Serial.println("Connection verified");
+  }
+}
+
+void verifyMQTT() {
+  if (!mqttClient.connected()) {
+    Serial.println("MQTT not verified");
+    Serial.println("NB Access:");
+    Serial.println(nbAccess.isAccessAlive());
+    Serial.println("MqttConnect:");
+    Serial.println(mqttClient.connected());
+    setupMQTT();
+  } else {
+    Serial.println("MQTT verified");
+  }
+}
+
+void restartSerial() {
+  if (!Serial) {
+    Serial.begin(9600);
+    while (!Serial && (millis() < 10000)) {
+    }
+  }
 }
